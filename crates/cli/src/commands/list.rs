ed11243745aa027cb1ae7eff991969dd3258e65c
Miles Johnson <mileswjohnson@gmail.com>
2023-03-01 15:31:01 -0800

    Improve logging.

--- crates/cli/src/commands/list.rs
+++ crates/cli/src/commands/list.rs
@@ -1,4 +1,5 @@
 use crate::helpers::enable_logging;
+use human_sort::compare;
 use log::{debug, info};
 use proto::{color, create_tool, ProtoError, ToolType};
 use std::fs;
@@ -8,28 +9,31 @@ pub async fn list(tool_type: ToolType) -> Result<(), ProtoError> {
 
     let tool = create_tool(&tool_type)?;
     let install_dir = tool.get_install_dir()?;
-    let tool_dir = install_dir.parent().unwrap(); // Without version
+    let tool_dir = install_dir.parent().unwrap(); // Without version/latest
 
     debug!(target: "proto:list", "Finding versions in {}", color::path(tool_dir));
 
-    info!(target: "proto:list", "Locally installed versions:");
-
     let handle_error = |e: std::io::Error| ProtoError::Fs(tool_dir.to_path_buf(), e.to_string());
-    let mut install_count = 0;
+    let mut versions = vec![];
 
     if tool_dir.exists() {
         for entry in fs::read_dir(tool_dir).map_err(handle_error)? {
             let entry = entry.map_err(handle_error)?;
 
             if entry.file_type().map_err(handle_error)?.is_dir() {
-                install_count += 1;
-                println!("{}", entry.file_name().to_string_lossy());
+                versions.push(entry.file_name().to_string_lossy().to_string());
             }
         }
     }
 
-    if install_count == 0 {
+    info!(target: "proto:list", "Locally installed versions:");
+
+    if versions.is_empty() {
         eprintln!("No versions installed");
+    } else {
+        versions.sort_by(|a, d| compare(&a, &d));
+
+        println!("{}", versions.join("\n"));
     }
 
     Ok(())
