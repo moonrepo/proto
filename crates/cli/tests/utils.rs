9f506ee78c2bdcf39ff6aa5fcb5d6efc8c58df1a
Miles Johnson <mileswjohnson@gmail.com>
2023-03-13 16:10:40 -0700

    Bump timeout.

--- crates/cli/tests/utils.rs
+++ crates/cli/tests/utils.rs
@@ -14,7 +14,7 @@ pub fn create_proto_command<T: AsRef<Path>>(path: T) -> assert_cmd::Command {
     let path = path.as_ref();
 
     let mut cmd = assert_cmd::Command::cargo_bin("proto").unwrap();
-    cmd.timeout(std::time::Duration::from_secs(90));
+    cmd.timeout(std::time::Duration::from_secs(120));
     cmd.current_dir(path);
     cmd.env("RUST_BACKTRACE", "1");
     cmd.env("PROTO_ROOT", path.as_os_str());
