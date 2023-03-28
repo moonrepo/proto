ca486b22375fb02052c1866af0f7eccce07921fd
Miles Johnson <mileswjohnson@gmail.com>
2023-03-28 11:27:09 -0700

    Add auto-install.

--- crates/cli/src/commands/run.rs
+++ crates/cli/src/commands/run.rs
@@ -1,4 +1,7 @@
+use crate::commands::install::install;
 use crate::tools::{create_tool, ToolType};
+use log::debug;
+use proto::UserConfig;
 use proto_core::{detect_version_from_environment, ProtoError};
 use std::process::exit;
 use tokio::process::Command;
@@ -12,11 +15,32 @@ pub async fn run(
     let version = detect_version_from_environment(&tool, forced_version).await?;
 
     if !tool.is_setup(&version).await? {
-        return Err(ProtoError::MissingToolForRun(
-            tool.get_name(),
-            version.to_owned(),
-            format!("proto install {} {}", tool.get_bin_name(), version),
-        ));
+        let config = UserConfig::load()?;
+
+        if !config.auto_install {
+            return Err(ProtoError::MissingToolForRun(
+                tool.get_name(),
+                version.to_owned(),
+                format!("proto install {} {}", tool.get_bin_name(), version),
+            ));
+        }
+
+        // Install the tool
+        debug!(
+            target: tool.get_log_target(),
+            "Auto-install setting is configured, attempting to install"
+        );
+
+        install(
+            tool_type,
+            Some(tool.get_resolved_version().to_owned()),
+            false,
+            vec![],
+        )
+        .await?;
+
+        // Find the new binaries
+        tool.find_bin_path().await?;
     }
 
     let status = Command::new(tool.get_bin_path()?)
