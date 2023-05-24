e622c2e3641641e819f2810b7229e43ce28ef552
Miles Johnson <mileswjohnson@gmail.com>
2023-05-23 21:34:00 -0700

    Create template.

--- crates/node/src/depman/shim.rs
+++ crates/node/src/depman/shim.rs
@@ -1,3 +1,4 @@
+use crate::depman::NodeDependencyManagerType;
 use crate::NodeDependencyManager;
 use proto_core::{
     async_trait, Executable, Installable, ProtoError, Resolvable, ShimBuilder, Shimable,
@@ -10,7 +11,7 @@ fn node_gyp_template() -> String {
 npm_bin=$(proto bin npm)
 
 # ...and then replace the npm bin with node-gyp. Simple but works!
-node_gyp_bin=$(echo "$npm_bin" | sed 's/bin\/npm-cli.js/bin\/node-gyp-bin\/node-gyp/')
+node_gyp_bin=$(echo "$npm_bin" | sed 's/bin\/npm/bin\/node-gyp-bin\/node-gyp/')
 
 exec "$node_gyp_bin" "$@""#
         .to_owned()
@@ -22,7 +23,7 @@ fn node_gyp_template() -> String {
 $NpmBin = proto.exe bin npm
 
 # ...and then replace the npm bin with node-gyp. Simple but works!
-$NodeGypBin = $NpmBin.replace("node.exe", "npx.cmd")
+$NodeGypBin = $NpmBin.replace("npm.cmd", "node-gyp-bin/node-gyp.cmd")
 
 & $NodeGypBin $args"#
         .to_owned()
@@ -41,6 +42,25 @@ impl Shimable<'_> for NodeDependencyManager {
         shimmer.create_global_shim()?;
 
         self.shim_path = Some(shimmer.create_tool_shim(find_only)?);
+
+        // node-gyp
+        if matches!(self.type_of, NodeDependencyManagerType::Npm) {
+            let mut shimmer = ShimBuilder::new(
+                "node-gyp",
+                &self
+                    .get_bin_path()?
+                    .parent()
+                    .unwrap()
+                    .join("node-gyp-bin/node-gyp"),
+            );
+
+            shimmer
+                .dir(self.get_install_dir()?)
+                .version(self.get_resolved_version())
+                .set_global_template(node_gyp_template());
+
+            shimmer.create_global_shim()?;
+        }
 
         Ok(())
     }
