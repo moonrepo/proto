103c52476cd5fda7afdbe94ff45b12fb9a9203f9
Miles Johnson <mileswjohnson@gmail.com>
2023-07-26 09:44:13 -0700

    Clean up.

--- crates/cli/src/commands/install_all.rs
+++ crates/cli/src/commands/install_all.rs
@@ -1,12 +1,11 @@
 use crate::helpers::{disable_progress_bars, enable_progress_bars};
-use crate::tools::{create_plugin_from_locator, create_tool, ToolType};
+use crate::tools::{create_plugin_from_locator, ToolType};
 use crate::{commands::clean::clean, commands::install::install, helpers::create_progress_bar};
 use futures::future::try_join_all;
 use proto_core::{expand_detected_version, Proto, ToolsConfig, UserConfig};
 use starbase::SystemResult;
 use std::env;
 use std::str::FromStr;
-use strum::IntoEnumIterator;
 use tracing::{debug, info};
 
 pub async fn install_all() -> SystemResult {
@@ -19,47 +18,24 @@ pub async fn install_all() -> SystemResult {
     config.inherit_builtin_plugins();
 
     // Detect from working dir
-    debug!("Detecting tools from environment");
-
-    for tool_type in ToolType::iter() {
-        if let ToolType::Plugin(_) = tool_type {
-            continue;
-        }
-
-        let tool = create_tool(&tool_type).await?;
-
-        if let Some(version) = tool.detect_version_from(&working_dir).await? {
-            if let Some(version) = expand_detected_version(&version, tool.get_manifest()?)? {
-                debug!(version, "Detected version for {}", tool.get_name());
-
-                config.tools.insert(tool.get_id().to_owned(), version);
-            }
-        }
-    }
+    debug!("Detecting tool versions from environment");
 
     let plugins = config.plugins;
 
     if !plugins.is_empty() {
         let proto = Proto::new()?;
-        let pb = create_progress_bar(format!(
-            "Installing {} plugins: {}",
-            plugins.len(),
-            plugins.keys().cloned().collect::<Vec<_>>().join(", ")
-        ));
 
-        for (name, locator) in &plugins {
-            let plugin = create_plugin_from_locator(name, &proto, locator).await?;
+        for (name, locator) in plugins {
+            let tool = create_plugin_from_locator(&name, &proto, &locator).await?;
 
-            if let Some(version) = plugin.detect_version_from(&working_dir).await? {
-                if let Some(version) = expand_detected_version(&version, plugin.get_manifest()?)? {
-                    debug!(version, "Detected version for {}", plugin.get_name());
+            if let Some(version) = tool.detect_version_from(&working_dir).await? {
+                if let Some(version) = expand_detected_version(&version, tool.get_manifest()?)? {
+                    debug!(version, "Detected version for {}", tool.get_name());
 
-                    config.tools.insert(plugin.get_id().to_owned(), version);
+                    config.tools.insert(tool.get_id().to_owned(), version);
                 }
             }
         }
-
-        pb.finish_and_clear();
     }
 
     let tools = config.tools;
@@ -90,7 +66,7 @@ pub async fn install_all() -> SystemResult {
         pb.finish_and_clear();
     }
 
-    if tools.is_empty() && plugins.is_empty() {
+    if tools.is_empty() {
         info!("Nothing to install!")
     } else {
         info!("Successfully installed tools and plugins");
