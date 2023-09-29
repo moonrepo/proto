37e79ea320b479d5006eba328cac22e987b82f37
Miles Johnson <mileswjohnson@gmail.com>
2023-09-29 13:32:33 -0700

    Add json support.

--- crates/cli/src/commands/outdated.rs
+++ crates/cli/src/commands/outdated.rs
@@ -1,17 +1,32 @@
 use clap::Args;
+use miette::IntoDiagnostic;
 use proto_core::{load_tool, ToolsConfig, UnresolvedVersionSpec, VersionSpec};
+use serde::Serialize;
 use starbase::system;
 use starbase_styles::color::{self, OwoStyle};
+use starbase_utils::json;
+use std::collections::HashMap;
 use std::process;
 use tracing::{debug, info};
 
 #[derive(Args, Clone, Debug)]
 pub struct OutdatedArgs {
+    #[arg(long, help = "Print the list in JSON format")]
+    json: bool,
+
     #[arg(
         long,
         help = "Check for latest version available ignoring requirements and ranges"
     )]
     latest: bool,
+}
+
+#[derive(Serialize)]
+pub struct OutdatedItem {
+    is_latest: bool,
+    version_config: UnresolvedVersionSpec,
+    current_version: VersionSpec,
+    newer_version: VersionSpec,
 }
 
 #[system]
@@ -24,7 +39,11 @@ pub async fn outdated(args: ArgsRef<OutdatedArgs>) {
         process::exit(1);
     }
 
-    info!("Checking for latest versions");
+    if !args.json {
+        info!("Checking for newer versions...");
+    }
+
+    let mut items = HashMap::new();
 
     for (tool_id, config_version) in &tools_config.tools {
         let mut tool = load_tool(tool_id).await?;
@@ -35,14 +54,15 @@ pub async fn outdated(args: ArgsRef<OutdatedArgs>) {
         let mut comments = vec![];
         let versions = tool.load_version_resolver(&initial_version).await?;
         let current_version = versions.resolve(config_version)?;
+        let is_latest = args.latest || matches!(config_version, UnresolvedVersionSpec::Version(_));
 
         comments.push(format!(
-            "{} {}",
-            color::muted_light("current version"),
-            color::symbol(current_version.to_string())
+            "current version {} {}",
+            color::symbol(current_version.to_string()),
+            color::muted_light(format!("(via {})", config_version))
         ));
 
-        let latest_version = versions.resolve_without_manifest(if args.latest {
+        let newer_version = versions.resolve_without_manifest(if is_latest {
             &initial_version // latest alias
         } else {
             config_version // req, range, etc
@@ -50,15 +70,15 @@ pub async fn outdated(args: ArgsRef<OutdatedArgs>) {
 
         comments.push(format!(
             "{} {}",
-            color::muted_light(if args.latest {
+            if is_latest {
                 "latest version"
             } else {
-                "updatable version"
-            }),
-            color::symbol(latest_version.to_string())
+                "newer version"
+            },
+            color::symbol(newer_version.to_string())
         ));
 
-        let is_outdated = match (current_version, latest_version) {
+        let is_outdated = match (&current_version, &newer_version) {
             (VersionSpec::Version(a), VersionSpec::Version(b)) => b > a,
             _ => false,
         };
@@ -67,11 +87,29 @@ pub async fn outdated(args: ArgsRef<OutdatedArgs>) {
             comments.push(color::success("update available!"));
         }
 
-        println!(
-            "{} {} {}",
-            OwoStyle::new().bold().style(color::id(&tool.id)),
-            color::muted("-"),
-            comments.join(&color::muted(" - "))
-        );
+        if args.json {
+            items.insert(
+                tool.id,
+                OutdatedItem {
+                    is_latest,
+                    version_config: config_version.to_owned(),
+                    current_version,
+                    newer_version,
+                },
+            );
+        } else {
+            println!(
+                "{} {} {}",
+                OwoStyle::new().bold().style(color::id(&tool.id)),
+                color::muted("-"),
+                comments.join(&color::muted_light(", "))
+            );
+        }
+    }
+
+    if args.json {
+        println!("{}", json::to_string_pretty(&items).into_diagnostic()?);
+
+        return Ok(());
     }
 }
