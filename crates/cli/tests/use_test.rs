cec7d4bdc39201271c17b66f634dc8aaf00cec37
Miles Johnson <mileswjohnson@gmail.com>
2023-12-07 13:17:22 -0800

    Add test.

--- crates/cli/tests/use_test.rs
+++ crates/cli/tests/use_test.rs
@@ -46,4 +46,23 @@ deno = "1.30.0"
 
         assert!(node_path.exists());
     }
+
+    #[test]
+    fn doesnt_install_global_tools() {
+        let sandbox = create_empty_sandbox();
+        let node_path = sandbox.path().join(".proto/tools/node/19.0.0");
+        let deno_path = sandbox.path().join(".proto/tools/deno/1.30.0");
+
+        sandbox.create_file(".prototools", r#"node = "19.0.0""#);
+        sandbox.create_file(".proto/.prototools", r#"deno = "1.30.0""#);
+
+        assert!(!node_path.exists());
+        assert!(!deno_path.exists());
+
+        let mut cmd = create_proto_command(sandbox.path());
+        cmd.arg("use").assert().success();
+
+        assert!(node_path.exists());
+        assert!(!deno_path.exists());
+    }
 }
