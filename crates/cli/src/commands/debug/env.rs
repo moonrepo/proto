f063266769107d89810f019097f04630ab7efb63
Miles Johnson <mileswjohnson@gmail.com>
2023-12-22 10:55:19 -0800

    Add command.

---
+++ crates/cli/src/commands/debug/env.rs
@@ -0,0 +1,84 @@
+use crate::helpers::ProtoResource;
+use crate::printer::Printer;
+use proto_pdk_api::{HostArch, HostOS};
+use starbase::system;
+use starbase_styles::color;
+use std::env;
+
+#[system]
+pub async fn env(proto: ResourceRef<ProtoResource>) {
+    let manager = proto.env.load_config_manager()?;
+    let mut printer = Printer::new();
+
+    // STORE
+
+    printer.named_section("Store", |p| {
+        p.entry("Root", color::path(&proto.env.root));
+        p.entry("Bins", color::path(&proto.env.bin_dir));
+        p.entry("Shims", color::path(&proto.env.shims_dir));
+        p.entry("Plugins", color::path(&proto.env.plugins_dir));
+        p.entry("Tools", color::path(&proto.env.tools_dir));
+        p.entry("Temp", color::path(&proto.env.temp_dir));
+        p.entry_map(
+            "Virtual",
+            proto
+                .env
+                .get_virtual_paths()
+                .iter()
+                .map(|(h, g)| (color::file(g.to_string_lossy()), color::path(h))),
+            None,
+        );
+        p.entry_list(
+            "Configs",
+            manager.files.iter().filter_map(|f| {
+                if f.exists {
+                    Some(color::path(&f.path))
+                } else {
+                    None
+                }
+            }),
+            None,
+        );
+
+        Ok(())
+    })?;
+
+    // ENV
+
+    printer.named_section("Environment", |p| {
+        p.entry(
+            "Proto version",
+            color::muted_light(env!("CARGO_PKG_VERSION")),
+        );
+        p.entry(
+            "Operating system",
+            color::muted_light(HostOS::from_env().to_string()),
+        );
+        p.entry(
+            "Architecture",
+            color::muted_light(HostArch::from_env().to_string()),
+        );
+        p.entry_map(
+            "Variables",
+            env::vars().filter_map(|(k, v)| {
+                if k.starts_with("PROTO_") {
+                    Some((
+                        color::id(k),
+                        if v.contains('/') {
+                            color::path(v)
+                        } else {
+                            color::muted_light(v)
+                        },
+                    ))
+                } else {
+                    None
+                }
+            }),
+            None,
+        );
+
+        Ok(())
+    })?;
+
+    printer.flush();
+}
