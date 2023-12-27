075a1f029c81169d3409ecdc39e39f4a193ade0d
Miles Johnson <mileswjohnson@gmail.com>
2023-12-26 22:06:12 -0800

    Handle access denied.

--- crates/shim/src/windows.rs
+++ crates/shim/src/windows.rs
@@ -30,6 +30,22 @@ pub fn get_shim_file_name(name: &str) -> String {
     get_exe_file_name(name)
 }
 
+macro_rules! handle_io_error {
+    ($expr:expr) => {
+         if let Err(error) = $expr {
+            // If we receive an "Access is denied" error, we should 
+            // exit early as there's no way around this, as this exe
+            // may be currently in use by another process. This happens
+            // consistently when ran through task runners (like moon).
+            if error.raw_os_error().is_some_and(|code| code == 5) {
+                return Ok(());
+            } else {
+                return Err(error);
+            }
+        }
+    };
+}
+
 // We can't remove or overwrite an executable that is currently running,
 // but we can rename it and create the new shim alongside it.
 // @see https://stackoverflow.com/a/7198760
@@ -43,11 +59,11 @@ pub fn create_shim(source_code: &[u8], shim_path: &Path, find_only: bool) -> io:
 
     // Rename the current exe
     if shim_path.exists() {
-        fs::rename(shim_path, &renamed_shim_path)?;
+        handle_io_error!(fs::rename(shim_path, &renamed_shim_path));
     }
 
     // Create the new exe
-    fs::write(shim_path, source_code)?;
+    handle_io_error!(fs::write(shim_path, source_code));
 
     // Attempt to remove the old exe (but don't fail)
     if renamed_shim_path.exists() {
