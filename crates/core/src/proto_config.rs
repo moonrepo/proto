c85917e3df47e94c8577dc3113afc0d76429e18c
Miles Johnson <milesj@users.noreply.github.com>
2024-01-04 14:09:49 -0800

    deps: Update plugins. (#378)

--- crates/core/src/proto_config.rs
+++ crates/core/src/proto_config.rs
@@ -184,7 +184,7 @@ impl ProtoConfig {
             self.plugins.insert(
                 Id::raw(SCHEMA_PLUGIN_KEY),
                 PluginLocator::SourceUrl {
-                    url: "https://github.com/moonrepo/schema-plugin/releases/download/v0.7.0/schema_plugin.wasm".into()
+                    url: "https://github.com/moonrepo/schema-plugin/releases/download/v0.7.1/schema_plugin.wasm".into()
                 }
             );
         }
