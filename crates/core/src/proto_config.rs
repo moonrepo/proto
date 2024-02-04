19b4a3816a384f8c0a237649f269c042b3fd3181
Miles Johnson <mileswjohnson@gmail.com>
2024-02-03 16:18:36 -0800

    Update node plugin.

--- crates/core/src/proto_config.rs
+++ crates/core/src/proto_config.rs
@@ -203,7 +203,7 @@ impl ProtoConfig {
             self.plugins.insert(
                 Id::raw("node"),
                 PluginLocator::SourceUrl {
-                    url: "https://github.com/moonrepo/node-plugin/releases/download/v0.9.0/node_plugin.wasm".into()
+                    url: "https://github.com/moonrepo/node-plugin/releases/download/v0.9.1/node_plugin.wasm".into()
                 }
             );
         }
@@ -213,7 +213,7 @@ impl ProtoConfig {
                 self.plugins.insert(
                     Id::raw(depman),
                     PluginLocator::SourceUrl {
-                        url: "https://github.com/moonrepo/node-plugin/releases/download/v0.9.0/node_depman_plugin.wasm".into()
+                        url: "https://github.com/moonrepo/node-plugin/releases/download/v0.9.1/node_depman_plugin.wasm".into()
                     }
                 );
             }
