c443639e070fbf5ebb8c9de89289c1b7fa78dcf1
Miles Johnson <mileswjohnson@gmail.com>
2024-04-18 16:00:33 -0700

    Update shell usage.

--- crates/cli/src/commands/setup.rs
+++ crates/cli/src/commands/setup.rs
@@ -1,12 +1,12 @@
 use crate::helpers::ProtoResource;
 use crate::shell::{
-    detect_shell, format_exports, prompt_for_shell_profile, write_profile,
+    format_exports, prompt_for_shell, prompt_for_shell_profile, write_profile,
     write_profile_if_not_setup, Export,
 };
 use clap::Args;
-use clap_complete::Shell;
 use proto_shim::get_exe_file_name;
 use starbase::system;
+use starbase_shell::{BoxedShell, ShellType};
 use starbase_styles::color;
 use std::env;
 use std::io::stdout;
@@ -20,17 +20,13 @@ mod windows;
 #[derive(Args, Clone, Debug)]
 pub struct SetupArgs {
     #[arg(long, help = "Shell to setup for")]
-    shell: Option<Shell>,
+    shell: Option<ShellType>,
 
-    #[arg(long, help = "Don't update a shell profile")]
-    no_profile: bool,
+    #[arg(long, help = "Don't update a shell profile", alias = "no-profile")]
+    no_modify_profile: bool,
 
     #[arg(long, help = "Don't update the system path")]
     no_modify_path: bool,
-
-    // deprecated
-    #[arg(long, hide = true, help = "Return the shell profile path if setup")]
-    profile: bool,
 
     #[arg(long, short = 'y', help = "Avoid interactive prompts and use defaults")]
     yes: bool,
@@ -38,7 +34,6 @@ pub struct SetupArgs {
 
 #[system]
 pub async fn setup(args: ArgsRef<SetupArgs>, proto: ResourceRef<ProtoResource>) {
-    let shell = detect_shell(args.shell);
     let paths = env::split_paths(&env::var("PATH").unwrap()).collect::<Vec<_>>();
 
     if paths.contains(&proto.env.store.shims_dir) && paths.contains(&proto.env.store.bin_dir) {
@@ -48,6 +43,21 @@ pub async fn setup(args: ArgsRef<SetupArgs>, proto: ResourceRef<ProtoResource>) 
 
         return Ok(());
     }
+
+    debug!("Determining the shell to use");
+
+    let interactive = !args.yes && env::var("CI").is_err() && stdout().is_terminal();
+
+    let shell_type = match args.shell.or_else(ShellType::detect) {
+        Some(value) => value,
+        None => {
+            debug!("Unable to detect, prompting the user to select a shell");
+
+            prompt_for_shell()?
+        }
+    };
+
+    let shell = shell_type.build();
 
     println!("Finishing proto installation...");
 
@@ -60,34 +70,37 @@ pub async fn setup(args: ArgsRef<SetupArgs>, proto: ResourceRef<ProtoResource>) 
         ],
     );
 
-    let profile_path = match content {
-        Some(ref content) if !args.no_profile => {
-            let interactive = !args.yes && env::var("CI").is_err() && stdout().is_terminal();
-
-            update_shell_profile(&shell, proto, content, interactive)?
-        }
-        _ => None,
-    };
-
-    #[cfg(windows)]
-    let system_path_was_updated = if !args.no_modify_path {
-        windows::do_add_to_path(vec![
-            proto.env.store.shims_dir.clone(),
-            proto.env.store.bin_dir.clone(),
-        ])?
+    let modified_profile_path = if args.no_modify_profile {
+        None
     } else {
-        false
+        update_shell_profile(&shell, proto, &content, interactive)?
     };
 
-    // unix makes no distinction here
-    #[cfg(unix)]
-    let system_path_was_updated = profile_path.is_some();
+    let modified_system_env_path = if args.no_modify_path {
+        false
+    } else {
+        #[cfg(windows)]
+        {
+            windows::do_add_to_path(vec![
+                proto.env.store.shims_dir.clone(),
+                proto.env.store.bin_dir.clone(),
+            ])?
+        }
 
-    finished_message(proto, content, profile_path, system_path_was_updated);
+        #[cfg(unix)]
+        true
+    };
+
+    finished_message(
+        proto,
+        content,
+        modified_profile_path,
+        modified_system_env_path,
+    );
 }
 
 fn update_shell_profile(
-    shell: &Shell,
+    shell: &BoxedShell,
     proto: &ProtoResource,
     content: &str,
     interactive: bool,
@@ -96,23 +109,20 @@ fn update_shell_profile(
 
     let profile_path;
 
-    // If interactive, let the user pick a profile
     if interactive {
         debug!("Prompting the user to select a shell profile");
 
-        profile_path = prompt_for_shell_profile(shell, &proto.env.cwd)?;
+        profile_path = prompt_for_shell_profile(shell, &proto.env.cwd, &proto.env.home)?;
 
         if let Some(profile) = &profile_path {
             debug!("Selected profile {}, updating", color::path(profile));
 
             write_profile(profile, content, "PROTO_HOME")?;
         }
-    }
-    // Otherwise attempt to find one
-    else {
+    } else {
         debug!("Attempting to find a shell profile to update");
 
-        profile_path = write_profile_if_not_setup(shell, content, "PROTO_HOME")?;
+        profile_path = write_profile_if_not_setup(shell, content, "PROTO_HOME", &proto.env.home)?;
     }
 
     // If we found a profile, update the global config so we can reference it
@@ -163,9 +173,9 @@ fn manual_system_path_message(proto: &ProtoResource) {
 
 fn finished_message(
     proto: &ProtoResource,
-    exported_content: Option<String>,
-    profile_path: Option<PathBuf>,
-    system_path_was_updated: bool,
+    exported_content: String,
+    modified_profile_path: Option<PathBuf>,
+    modified_system_env_path: bool,
 ) {
     let installed_bin_path = proto.env.store.bin_dir.join(get_exe_file_name("proto"));
 
@@ -174,13 +184,13 @@ fn finished_message(
         color::path(installed_bin_path),
     );
 
-    profile_path.as_ref().inspect(|path| {
+    modified_profile_path.as_ref().inspect(|path| {
         println!("The shell profile at {} was updated.", color::path(path));
     });
 
-    if system_path_was_updated || profile_path.is_some() {
+    if modified_system_env_path || modified_profile_path.is_some() {
         println!("Launch a new terminal window to start using proto!");
-    } else if exported_content.is_some() && profile_path.is_none() {
+    } else if !exported_content.is_empty() && modified_profile_path.is_none() {
         if cfg!(windows) {
             manual_system_path_message(proto);
             println!();
@@ -189,7 +199,7 @@ fn finished_message(
             println!("Add the following to your shell profile to get started:");
         }
         println!();
-        println!("{}", color::muted_light(exported_content.unwrap().trim()));
+        println!("{}", color::muted_light(exported_content.trim()));
     } else {
         manual_system_path_message(proto);
     }
--- crates/cli/src/shell.rs
+++ crates/cli/src/shell.rs
@@ -1,172 +1,33 @@
-use clap_complete::Shell;
+use crate::helpers::create_theme;
 use dialoguer::{Input, Select};
-use dirs::{config_dir, document_dir, home_dir};
 use miette::IntoDiagnostic;
-use proto_core::ENV_VAR;
+use starbase_shell::{BoxedShell, ShellType};
 use starbase_styles::color;
 use starbase_utils::fs;
 use std::{
-    env,
+    env::{self, consts},
     io::{self, BufRead},
     path::{Path, PathBuf},
 };
 use tracing::debug;
-
-use crate::helpers::create_theme;
 
 pub enum Export {
     Path(Vec<String>),
     Var(String, String),
 }
 
-pub fn detect_shell(shell: Option<Shell>) -> Shell {
-    shell.or_else(Shell::from_env).unwrap_or({
-        if cfg!(windows) {
-            Shell::PowerShell
-        } else {
-            Shell::Bash
-        }
-    })
-}
-
-pub fn find_profiles(shell: &Shell) -> miette::Result<Vec<PathBuf>> {
+pub fn find_profiles(shell: &BoxedShell, home_dir: &Path) -> miette::Result<Vec<PathBuf>> {
     debug!("Finding profile files for {}", shell);
 
     if let Ok(profile_env) = env::var("PROTO_SHELL_PROFILE") {
         return Ok(vec![PathBuf::from(profile_env)]);
     }
 
-    let home_dir = home_dir().expect("Invalid home directory.");
-    let mut profiles = vec![home_dir.join(".profile")];
-
-    if let Ok(profile_env) = env::var("PROFILE") {
-        if !profile_env.is_empty() {
-            profiles.push(PathBuf::from(profile_env));
-        }
-    }
-
-    match shell {
-        Shell::Bash => {
-            profiles.extend([home_dir.join(".bash_profile"), home_dir.join(".bashrc")]);
-        }
-        Shell::Elvish => {
-            profiles.push(home_dir.join(".elvish/rc.elv"));
-
-            if let Some(dir) = config_dir() {
-                profiles.push(dir.join("elvish/rc.elv"));
-            }
-
-            if cfg!(unix) {
-                profiles.push(home_dir.join(".config/elvish/rc.elv"));
-            }
-        }
-        Shell::Fish => {
-            profiles.push(home_dir.join(".config/fish/config.fish"));
-        }
-        Shell::PowerShell => {
-            if cfg!(windows) {
-                let docs_dir = document_dir().unwrap_or(home_dir.join("Documents"));
-
-                profiles.extend([
-                    docs_dir.join("PowerShell\\Microsoft.PowerShell_profile.ps1"),
-                    docs_dir.join("PowerShell\\Profile.ps1"),
-                ]);
-            } else {
-                profiles.extend([
-                    home_dir.join(".config/powershell/Microsoft.PowerShell_profile.ps1"),
-                    home_dir.join(".config/powershell/profile.ps1"),
-                ]);
-            }
-        }
-        Shell::Zsh => {
-            let zdot_dir = env::var("ZDOTDIR").map(PathBuf::from).unwrap_or(home_dir);
-
-            profiles.extend([
-                zdot_dir.join(".zshenv"),
-                zdot_dir.join(".zprofile"),
-                zdot_dir.join(".zshrc"),
-            ]);
-        }
-        _ => {}
-    };
-
-    Ok(profiles)
+    Ok(shell.get_profile_paths(&home_dir))
 }
 
-pub fn format_export(shell: &Shell, var: Export, newline: &str) -> Option<String> {
-    let result = match shell {
-        Shell::Bash | Shell::Zsh => match var {
-            Export::Path(paths) => format!(r#"export PATH="{}:$PATH""#, paths.join(":")),
-            Export::Var(key, value) => format!(r#"export {key}="{value}""#),
-        },
-        Shell::Elvish => {
-            fn format(value: String) -> String {
-                ENV_VAR
-                    .replace_all(&value, "$$E:$name")
-                    .replace("$E:HOME", "{~}")
-            }
-
-            match var {
-                Export::Path(paths) => format!("set paths [{} $@paths]", format(paths.join(" "))),
-                Export::Var(key, value) => format!("set-env {key} {}", format(value)),
-            }
-        }
-        Shell::Fish => match var {
-            Export::Path(paths) => format!(r#"set -gx PATH "{}" $PATH"#, paths.join(":")),
-            Export::Var(key, value) => format!(r#"set -gx {key} "{value}""#),
-        },
-        Shell::PowerShell => {
-            fn format(value: String) -> String {
-                ENV_VAR
-                    .replace_all(&value, "$$env:$name")
-                    .replace("$env:HOME", "$HOME")
-            }
-
-            fn join_path(value: String) -> String {
-                let parts = value
-                    .split('/')
-                    .map(|part| {
-                        if part.starts_with('$') {
-                            part.to_owned()
-                        } else {
-                            format!("\"{}\"", part)
-                        }
-                    })
-                    .collect::<Vec<_>>();
-
-                format(format!("Join-Path {}", parts.join(" ")))
-            }
-
-            match var {
-                Export::Path(paths) => {
-                    let mut value = format!("$env:PATH = @({newline}");
-
-                    for path in paths {
-                        value.push_str(&format!("  ({}),{newline}", join_path(path)))
-                    }
-
-                    value.push_str("  $env:PATH");
-                    value.push_str(newline);
-                    value.push_str(") -join [IO.PATH]::PathSeparator");
-                    value
-                }
-                Export::Var(key, value) => {
-                    if value.contains('/') {
-                        format!("$env:{key} = {}", join_path(value))
-                    } else {
-                        format!(r#"$env:{key} = "{}""#, format(value))
-                    }
-                }
-            }
-        }
-        _ => return None,
-    };
-
-    Some(result)
-}
-
-pub fn format_exports(shell: &Shell, comment: &str, exports: Vec<Export>) -> Option<String> {
-    let newline = if matches!(shell, Shell::PowerShell) {
+pub fn format_exports(shell: &BoxedShell, comment: &str, exports: Vec<Export>) -> String {
+    let newline = if consts::OS == "windows" {
         "\r\n"
     } else {
         "\n"
@@ -174,31 +35,32 @@ pub fn format_exports(shell: &Shell, comment: &str, exports: Vec<Export>) -> Opt
     let mut lines = vec![format!("{newline}# {comment}")];
 
     for export in exports {
-        match format_export(shell, export, newline) {
-            Some(var) => lines.push(var),
-            None => return None,
-        };
+        lines.push(match export {
+            Export::Path(paths) => shell.format_path_export(&paths),
+            Export::Var(key, value) => shell.format_env_export(&key, &value),
+        });
     }
 
-    Some(lines.join(newline))
+    lines.join(newline)
 }
 
 pub fn write_profile(profile: &Path, contents: &str, env_var: &str) -> miette::Result<PathBuf> {
     fs::append_file(profile, contents)?;
 
-    debug!("Setup profile {} with {}", color::path(profile), env_var,);
+    debug!("Setup profile {} with {}", color::path(profile), env_var);
 
     Ok(profile.to_path_buf())
 }
 
 pub fn write_profile_if_not_setup(
-    shell: &Shell,
+    shell: &BoxedShell,
     contents: &str,
     env_var: &str,
+    home_dir: &Path,
 ) -> miette::Result<Option<PathBuf>> {
-    let profiles = find_profiles(shell)?;
+    let profiles = find_profiles(shell, home_dir)?;
 
-    for profile in &profiles {
+    for profile in profiles {
         debug!("Checking if shell profile {} exists", color::path(profile));
 
         if !profile.exists() {
@@ -229,22 +91,41 @@ pub fn write_profile_if_not_setup(
         debug!("Not setup, continuing");
     }
 
-    // Create a profile if none found. Use the last profile in the list
-    // as it's the "most common", and is typically the interactive shell.
-    let last_profile = profiles.last().unwrap();
+    // If no profile found, use the env-specific profile for the shell
+    let last_profile = shell.get_env_path(home_dir);
 
     debug!(
         "Found no configured profile, updating {}",
         color::path(last_profile),
     );
 
-    Ok(Some(write_profile(last_profile, contents, env_var)?))
+    Ok(Some(write_profile(&last_profile, contents, env_var)?))
 }
 
-pub fn prompt_for_shell_profile(shell: &Shell, cwd: &Path) -> miette::Result<Option<PathBuf>> {
+pub fn prompt_for_shell() -> miette::Result<ShellType> {
+    let theme = create_theme();
+    let items = ShellType::os_variants();
+
+    let default_index = 0;
+    let selected_index = Select::with_theme(&theme)
+        .with_prompt("Which shell to use?")
+        .items(&items)
+        .default(default_index)
+        .interact_opt()
+        .into_diagnostic()?
+        .unwrap_or(default_index);
+
+    Ok(items[selected_index])
+}
+
+pub fn prompt_for_shell_profile(
+    shell: &BoxedShell,
+    working_dir: &Path,
+    home_dir: &Path,
+) -> miette::Result<Option<PathBuf>> {
     let theme = create_theme();
 
-    let mut profiles = find_profiles(shell)?;
+    let mut profiles = find_profiles(shell, home_dir)?;
     profiles.reverse();
 
     let mut items = profiles.iter().map(color::path).collect::<Vec<_>>();
@@ -276,73 +157,11 @@ pub fn prompt_for_shell_profile(shell: &Shell, cwd: &Path) -> miette::Result<Opt
         Some(if custom_path.is_absolute() {
             custom_path
         } else {
-            cwd.join(custom_path)
+            working_dir.join(custom_path)
         })
     } else {
         Some(profiles[selected_index].clone())
     };
 
     Ok(selected_profile)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    fn get_env_vars() -> Vec<Export> {
-        vec![
-            Export::Var("PROTO_HOME".into(), "$HOME/.proto".into()),
-            Export::Path(vec!["$PROTO_HOME/shims".into(), "$PROTO_HOME/bin".into()]),
-        ]
-    }
-
-    #[test]
-    fn formats_bash_env_vars() {
-        assert_eq!(
-            format_exports(&Shell::Bash, "Bash", get_env_vars()).unwrap(),
-            r#"
-# Bash
-export PROTO_HOME="$HOME/.proto"
-export PATH="$PROTO_HOME/shims:$PROTO_HOME/bin:$PATH""#
-        );
-    }
-
-    #[test]
-    fn formats_elvish_env_vars() {
-        assert_eq!(
-            format_exports(&Shell::Elvish, "Elvish", get_env_vars()).unwrap(),
-            r#"
-# Elvish
-set-env PROTO_HOME {~}/.proto
-set paths [$E:PROTO_HOME/shims $E:PROTO_HOME/bin $@paths]"#
-        );
-    }
-
-    #[test]
-    fn formats_fish_env_vars() {
-        assert_eq!(
-            format_exports(&Shell::Fish, "Fish", get_env_vars()).unwrap(),
-            r#"
-# Fish
-set -gx PROTO_HOME "$HOME/.proto"
-set -gx PATH "$PROTO_HOME/shims:$PROTO_HOME/bin" $PATH"#
-        );
-    }
-
-    #[test]
-    fn formats_pwsh_env_vars() {
-        assert_eq!(
-            format_exports(&Shell::PowerShell, "PowerShell", get_env_vars())
-                .unwrap()
-                .replace("\r\n", "\n"),
-            r#"
-# PowerShell
-$env:PROTO_HOME = Join-Path $HOME ".proto"
-$env:PATH = @(
-  (Join-Path $env:PROTO_HOME "shims"),
-  (Join-Path $env:PROTO_HOME "bin"),
-  $env:PATH
-) -join [IO.PATH]::PathSeparator"#
-        );
-    }
 }
