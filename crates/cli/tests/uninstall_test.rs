cf7123252e20fe3a9b69c5f7dea99efe0b931e74
Miles Johnson <mileswjohnson@gmail.com>
2024-12-18 16:38:06 -0800

    Update tests.

--- crates/cli/tests/uninstall_test.rs
+++ crates/cli/tests/uninstall_test.rs
@@ -43,6 +43,19 @@ mod uninstall {
     }
 
     #[test]
+    fn doesnt_uninstall_all_if_doesnt_exist() {
+        let sandbox = create_empty_proto_sandbox();
+
+        let assert = sandbox.run_bin(|cmd| {
+            cmd.arg("uninstall").arg("node");
+        });
+
+        assert.inner.stdout(predicate::str::contains(
+            "Node.js has not been installed locally",
+        ));
+    }
+
+    #[test]
     fn uninstalls_everything() {
         let sandbox = create_empty_proto_sandbox();
 
@@ -91,5 +104,64 @@ mod uninstall {
             std::fs::read_to_string(sandbox.path().join(".prototools")).unwrap(),
             ""
         );
+    }
+
+    #[allow(deprecated)]
+    #[cfg(not(windows))]
+    #[test]
+    fn removes_tool_bins() {
+        let sandbox = create_empty_proto_sandbox();
+        sandbox.create_file(".proto/tools/node/1.2.3/fake/file", "");
+        sandbox.create_file(
+            ".proto/tools/node/manifest.json",
+            r#"{ "installed_versions": ["1.2.3"] }"#,
+        );
+        sandbox.create_file(".proto/bin/other", "");
+
+        let bin1 = sandbox.path().join(".proto/bin/node");
+        let bin2 = sandbox.path().join(".proto/bin/node-1");
+        let bin3 = sandbox.path().join(".proto/bin/node-1.2");
+        let src = sandbox.path().join(".proto/tools/node/1.2.3/fake/file");
+
+        std::fs::soft_link(&src, &bin1).unwrap();
+        std::fs::soft_link(&src, &bin2).unwrap();
+        std::fs::soft_link(&src, &bin3).unwrap();
+
+        sandbox
+            .run_bin(|cmd| {
+                cmd.arg("uninstall").arg("--yes").arg("node");
+            })
+            .success();
+
+        assert!(!bin1.exists());
+        assert!(bin1.symlink_metadata().is_err());
+        assert!(!bin2.exists());
+        assert!(bin2.symlink_metadata().is_err());
+        assert!(!bin3.exists());
+        assert!(bin3.symlink_metadata().is_err());
+    }
+
+    #[test]
+    fn removes_tool_shims() {
+        let sandbox = create_empty_proto_sandbox();
+        sandbox.create_file(".proto/tools/npm/manifest.json", "{}");
+        sandbox.create_file(".proto/shims/npm", "");
+        sandbox.create_file(".proto/shims/npm.exe", "");
+        sandbox.create_file(".proto/shims/npx", "");
+        sandbox.create_file(".proto/shims/npx.exe", "");
+
+        sandbox
+            .run_bin(|cmd| {
+                cmd.arg("uninstall").arg("--yes").arg("npm");
+            })
+            .success();
+
+        if cfg!(windows) {
+            assert!(!sandbox.path().join(".proto/shims/npm.exe").exists());
+            assert!(!sandbox.path().join(".proto/shims/npx.exe").exists());
+        } else {
+            assert!(!sandbox.path().join(".proto/shims/npm").exists());
+            assert!(!sandbox.path().join(".proto/shims/npx").exists());
+        }
     }
 }
