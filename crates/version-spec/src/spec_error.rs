87f7ebddb8a46871bb15f5bfcddfbdfaea3adf41
Miles Johnson <mileswjohnson@gmail.com>
2025-02-07 15:23:49 -0800

    Update version spec.

--- crates/version-spec/src/spec_error.rs
+++ crates/version-spec/src/spec_error.rs
@@ -1,19 +1,19 @@
 #[derive(thiserror::Error, Debug)]
 pub enum SpecError {
     #[error("Invalid calver (calendar version) format.")]
-    CalverInvalidFormat,
+    InvalidCalverFormat,
 
-    #[error("Unknown version format `{0}`. Must be a semantic or calendar based format.")]
-    ResolvedUnknownFormat(String),
-
-    #[error("Requirement operator found in an invalid position")]
-    ParseInvalidReq,
-
-    #[error("Unknown character `{0}` in version string!")]
-    ParseUnknownChar(char),
+    #[error("Requirement operator found in an invalid position.")]
+    InvalidParseRequirement,
 
     #[error("Missing major number for semantic versions, or year for calendar versions.")]
-    ParseMissingMajorPart,
+    MissingParseMajorPart,
+
+    #[error("Unknown version format `{0}`. Must be a semantic or calendar based format.")]
+    UnknownResolvedFormat(String),
+
+    #[error("Unknown character `{0}` in version string!")]
+    UnknownParseChar(char),
 
     #[error(transparent)]
     Semver(#[from] semver::Error),
