e2d69fbfc950ec8d924d75a35c6b560352395b57
Miles Johnson <mileswjohnson@gmail.com>
2025-04-13 18:37:19 -0700

    Add to inventory.

--- crates/core/src/lockfile/v1.rs
+++ crates/core/src/lockfile/v1.rs
@@ -1,9 +1,12 @@
 use crate::checksum::ProtoChecksumError;
+use crate::helpers::{read_json_file_with_lock, write_json_file_with_lock};
 use crate::tool_spec::Backend;
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
-use std::fmt;
+use std::fmt::{self, Debug};
+use std::path::{Path, PathBuf};
 use std::str::FromStr;
+use tracing::{debug, instrument};
 use version_spec::VersionSpec;
 
 pub const LOCKFILE_NAME: &str = "lockfile.json";
@@ -56,7 +59,7 @@ impl Into<String> for LockfileChecksum {
     }
 }
 
-#[derive(Debug, Default, Deserialize, Serialize)]
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
 #[serde(default, deny_unknown_fields)]
 pub struct LockfileRecord {
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -72,8 +75,43 @@ pub struct LockfileRecord {
     pub suffix: Option<String>,
 }
 
-#[derive(Debug, Default, Deserialize, Serialize)]
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
 #[serde(default, deny_unknown_fields)]
 pub struct Lockfile {
     pub installed: BTreeMap<VersionSpec, LockfileRecord>,
+
+    #[serde(skip)]
+    pub path: PathBuf,
+}
+
+impl Lockfile {
+    pub fn load_from<P: AsRef<Path>>(dir: P) -> miette::Result<Self> {
+        Self::load(dir.as_ref().join(LOCKFILE_NAME))
+    }
+
+    #[instrument(name = "load_tool_lockfile")]
+    pub fn load<P: AsRef<Path> + Debug>(path: P) -> miette::Result<Self> {
+        let path = path.as_ref();
+
+        debug!(file = ?path, "Loading lockfile");
+
+        let mut manifest: Lockfile = if path.exists() {
+            read_json_file_with_lock(path)?
+        } else {
+            Lockfile::default()
+        };
+
+        path.clone_into(&mut manifest.path);
+
+        Ok(manifest)
+    }
+
+    #[instrument(name = "save_tool_lockfile", skip(self))]
+    pub fn save(&self) -> miette::Result<()> {
+        debug!(file = ?self.path, "Saving lockfile");
+
+        write_json_file_with_lock(&self.path, self)?;
+
+        Ok(())
+    }
 }
