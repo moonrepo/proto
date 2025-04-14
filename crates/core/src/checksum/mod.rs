e9547ce9b29a6f8d79bcb13f855829d3efb81769
Miles Johnson <mileswjohnson@gmail.com>
2025-04-14 08:16:08 -0700

    Move checksum.

--- crates/core/src/checksum/mod.rs
+++ crates/core/src/checksum/mod.rs
@@ -2,8 +2,12 @@ mod checksum_error;
 mod minisign;
 mod sha256;
 
-pub use checksum_error::*;
+use serde::{Deserialize, Serialize};
+use std::fmt;
 use std::path::Path;
+use std::str::FromStr;
+
+pub use checksum_error::*;
 
 #[tracing::instrument(skip_all)]
 pub fn verify_checksum(
@@ -18,5 +22,53 @@ pub fn verify_checksum(
             checksum_public_key.ok_or(ProtoChecksumError::MissingChecksumPublicKey)?,
         ),
         _ => sha256::verify_checksum(download_file, checksum_file),
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(into = "String", try_from = "String")]
+pub enum ChecksumRecord {
+    Minisign(String),
+    Sha256(String),
+}
+
+impl FromStr for ChecksumRecord {
+    type Err = ProtoChecksumError;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        match value.split_once(':') {
+            Some((pre, suf)) => match pre {
+                "minisign" => Ok(Self::Minisign(suf.to_owned())),
+                "sha256" => Ok(Self::Sha256(suf.to_owned())),
+                _ => Err(ProtoChecksumError::UnknownChecksumType {
+                    kind: pre.to_owned(),
+                }),
+            },
+            None => Err(ProtoChecksumError::MissingChecksumType),
+        }
+    }
+}
+
+impl TryFrom<String> for ChecksumRecord {
+    type Error = ProtoChecksumError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        Self::from_str(&value)
+    }
+}
+
+impl fmt::Display for ChecksumRecord {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Minisign(hash) => write!(f, "minisign:{hash}"),
+            Self::Sha256(hash) => write!(f, "sha256:{hash}"),
+        }
+    }
+}
+
+#[allow(clippy::from_over_into)]
+impl Into<String> for ChecksumRecord {
+    fn into(self) -> String {
+        self.to_string()
     }
 }
--- crates/core/src/lockfile/v1.rs
+++ crates/core/src/lockfile/v1.rs
@@ -5,59 +5,10 @@ use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 use std::fmt::{self, Debug};
 use std::path::{Path, PathBuf};
-use std::str::FromStr;
 use tracing::{debug, instrument};
 use version_spec::VersionSpec;
 
 pub const LOCKFILE_NAME: &str = "lockfile.json";
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-#[serde(into = "String", try_from = "String")]
-pub enum LockfileChecksum {
-    Minisign(String),
-    Sha256(String),
-}
-
-impl FromStr for LockfileChecksum {
-    type Err = ProtoChecksumError;
-
-    fn from_str(value: &str) -> Result<Self, Self::Err> {
-        match value.split_once(':') {
-            Some((pre, suf)) => match pre {
-                "minisign" => Ok(Self::Minisign(suf.to_owned())),
-                "sha256" => Ok(Self::Sha256(suf.to_owned())),
-                _ => Err(ProtoChecksumError::UnknownChecksumType {
-                    kind: pre.to_owned(),
-                }),
-            },
-            None => Err(ProtoChecksumError::MissingChecksumType),
-        }
-    }
-}
-
-impl TryFrom<String> for LockfileChecksum {
-    type Error = ProtoChecksumError;
-
-    fn try_from(value: String) -> Result<Self, Self::Error> {
-        Self::from_str(&value)
-    }
-}
-
-impl fmt::Display for LockfileChecksum {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Minisign(hash) => write!(f, "minisign:{hash}"),
-            Self::Sha256(hash) => write!(f, "sha256:{hash}"),
-        }
-    }
-}
-
-#[allow(clippy::from_over_into)]
-impl Into<String> for LockfileChecksum {
-    fn into(self) -> String {
-        self.to_string()
-    }
-}
 
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 #[serde(default, deny_unknown_fields)]
