1612c65d50ede06bc0f0c62c632cc0509565c9a3
Miles Johnson <mileswjohnson@gmail.com>
2025-04-18 23:06:26 -0700

    Support Get-FileHash.

--- crates/core/src/checksum/sha256.rs
+++ crates/core/src/checksum/sha256.rs
@@ -37,6 +37,10 @@ pub fn verify_checksum(
         .lines()
         .map_while(Result::ok)
     {
+        if line.is_empty() {
+            continue;
+        }
+
         // <checksum>  <file>
         // <checksum> *<file>
         // <checksum>
@@ -44,6 +48,17 @@ pub fn verify_checksum(
             || (line.starts_with(checksum_hash) && line.ends_with(&download_file_name))
         {
             return Ok(true);
+        }
+
+        // Checksum files on Windows are created with Get-FileHash,
+        // which has a different file structure than Unix
+        // https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-filehash?view=powershell-7.5
+        if line.starts_with("Hash") {
+            if let Some((_, hash)) = line.split_once(':') {
+                // The hash is all uppercase in the checksum file,
+                // but the one's we generate are not, so lowercase
+                return Ok(hash.trim().to_lowercase() == checksum_hash);
+            }
         }
     }
 
