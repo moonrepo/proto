2ddb2b2a90890e4673199e771e5725b6d6828a66
Miles Johnson <mileswjohnson@gmail.com>
2025-06-02 16:07:49 -0700

    Update config.

--- crates/core/src/config_error.rs
+++ crates/core/src/config_error.rs
@@ -1,11 +1,23 @@
-use miette::Diagnostic;
+use schematic::ConfigError;
 use starbase_styles::{Style, Stylize};
+use starbase_utils::fs::FsError;
+use starbase_utils::toml::TomlError;
 use std::path::PathBuf;
 use thiserror::Error;
 
-#[derive(Error, Debug, Diagnostic)]
+#[derive(Error, Debug)]
+#[cfg_attr(feature = "miette", derive(miette::Diagnostic))]
 pub enum ProtoConfigError {
-    #[diagnostic(code(proto::config::env_parse_failed))]
+    #[error(transparent)]
+    Fs(#[from] Box<FsError>),
+
+    #[error(transparent)]
+    Schematic(#[from] Box<ConfigError>),
+
+    #[error(transparent)]
+    Toml(#[from] Box<TomlError>),
+
+    #[cfg_attr(feature = "miette", diagnostic(code(proto::config::env_parse_failed)))]
     #[error(
         "Failed to parse .env file {}.",
         .path.style(Style::Path),
@@ -16,7 +28,7 @@ pub enum ProtoConfigError {
         error: Box<dotenvy::Error>,
     },
 
-    #[diagnostic(code(proto::config::missing_env_file))]
+    #[cfg_attr(feature = "miette", diagnostic(code(proto::config::missing_env_file)))]
     #[error(
         "The .env file {} does not exist. This was configured as {} in the config {}.",
         .path.style(Style::Path),
@@ -28,4 +40,22 @@ pub enum ProtoConfigError {
         config: String,
         config_path: PathBuf,
     },
+}
+
+impl From<FsError> for ProtoConfigError {
+    fn from(e: FsError) -> ProtoConfigError {
+        ProtoConfigError::Fs(Box::new(e))
+    }
+}
+
+impl From<ConfigError> for ProtoConfigError {
+    fn from(e: ConfigError) -> ProtoConfigError {
+        ProtoConfigError::Schematic(Box::new(e))
+    }
+}
+
+impl From<TomlError> for ProtoConfigError {
+    fn from(e: TomlError) -> ProtoConfigError {
+        ProtoConfigError::Toml(Box::new(e))
+    }
 }
