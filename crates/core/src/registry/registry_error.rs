059dd68c4316459d6b22cd2773314172e274e8f9
Miles Johnson <mileswjohnson@gmail.com>
2025-06-02 15:50:18 -0700

    Update registry.

--- crates/core/src/registry/registry_error.rs
+++ crates/core/src/registry/registry_error.rs
@@ -1,21 +1,41 @@
-use miette::Diagnostic;
 use starbase_styles::{Style, Stylize};
+use starbase_utils::fs::FsError;
+use starbase_utils::json::JsonError;
 use thiserror::Error;
 
-#[derive(Error, Debug, Diagnostic)]
+#[derive(Error, Debug)]
+#[cfg_attr(feature = "miette", derive(miette::Diagnostic))]
 pub enum ProtoRegistryError {
-    #[diagnostic(code(proto::registry::parse_failed))]
+    #[error(transparent)]
+    Fs(#[from] Box<FsError>),
+
+    #[error(transparent)]
+    Json(#[from] Box<JsonError>),
+
+    #[cfg_attr(feature = "miette", diagnostic(code(proto::registry::parse_failed)))]
     #[error("Failed to parse registry plugin data.")]
     FailedParse {
         #[source]
         error: Box<reqwest::Error>,
     },
 
-    #[diagnostic(code(proto::registry::request_failed))]
+    #[cfg_attr(feature = "miette", diagnostic(code(proto::registry::request_failed)))]
     #[error("Failed to request plugins from registry {}.", .url.style(Style::Url))]
     FailedRequest {
         url: String,
         #[source]
         error: Box<reqwest::Error>,
     },
+}
+
+impl From<FsError> for ProtoRegistryError {
+    fn from(e: FsError) -> ProtoRegistryError {
+        ProtoRegistryError::Fs(Box::new(e))
+    }
+}
+
+impl From<JsonError> for ProtoRegistryError {
+    fn from(e: JsonError) -> ProtoRegistryError {
+        ProtoRegistryError::Json(Box::new(e))
+    }
 }
