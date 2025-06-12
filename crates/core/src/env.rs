d1d340ed10ce10d2fa389f4fd128043402eb1ed3
Miles Johnson <mileswjohnson@gmail.com>
2025-06-12 12:20:06 -0700

    Add lock loading.

--- crates/core/src/env.rs
+++ crates/core/src/env.rs
@@ -141,7 +141,7 @@ impl ProtoEnvironment {
         }
     }
 
-    pub fn load_config_files(&self) -> Result<Vec<&ProtoFile>, ProtoConfigError> {
+    pub fn load_config_files(&self) -> Result<Vec<&ProtoConfigFile>, ProtoConfigError> {
         Ok(self
             .load_file_manager()?
             .entries
