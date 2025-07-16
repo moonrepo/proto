102dd455b9b711ace9a7903a1c4cece6f87186ee
Miles Johnson <mileswjohnson@gmail.com>
2025-07-16 15:06:27 -0700

    Polish.

--- crates/warpgate-pdk/src/tracing.rs
+++ crates/warpgate-pdk/src/tracing.rs
@@ -1,17 +1,17 @@
 use crate::funcs::get_plugin_id;
 use extism_pdk::{debug, error, info, trace, warn};
 use std::fmt;
-use tracing::{Event, Metadata, Subscriber, field::Visit, subscriber::set_global_default};
+use tracing::{Event, Level, Metadata, Subscriber, field::Visit, subscriber::set_global_default};
 use tracing_subscriber::{Layer, Registry, layer::Context, prelude::*};
 
 pub use tracing::level_filters::LevelFilter;
 
-struct FieldVisitor {
+struct EventMessage {
     message: String,
     fields: Vec<String>,
 }
 
-impl FieldVisitor {
+impl EventMessage {
     pub fn new(meta: &'static Metadata<'static>) -> Self {
         let mut visitor = Self {
             message: String::new(),
@@ -26,15 +26,7 @@ impl FieldVisitor {
     }
 }
 
-impl Visit for FieldVisitor {
-    fn record_str(&mut self, field: &tracing::field::Field, value: &str) {
-        if field.name() == "message" {
-            self.message = value.to_owned();
-        } else {
-            self.record_debug(field, &value)
-        }
-    }
-
+impl Visit for EventMessage {
     fn record_debug(&mut self, field: &tracing::field::Field, value: &dyn std::fmt::Debug) {
         if field.name() == "message" {
             self.message = format!("{value:?}");
@@ -44,7 +36,7 @@ impl Visit for FieldVisitor {
     }
 }
 
-impl fmt::Display for FieldVisitor {
+impl fmt::Display for EventMessage {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.message)?;
 
@@ -59,7 +51,7 @@ impl fmt::Display for FieldVisitor {
 /// Options to customize `tracing` handling within Extism.
 #[derive(Default)]
 pub struct WarpgateTracingOptions {
-    /// The minimum log level.
+    /// The minimum log level. Lower levels will be filtered.
     /// If not defined, logs all levels.
     pub level: Option<LevelFilter>,
 
@@ -99,20 +91,26 @@ impl<S: Subscriber> Layer<S> for WarpgateToExtismLayer {
     fn on_event(&self, event: &Event<'_>, _ctx: Context<'_, S>) {
         let meta = event.metadata();
         let level = meta.level();
-        let mut visitor = FieldVisitor::new(meta);
 
-        event.record(&mut visitor);
+        let mut message = EventMessage::new(meta);
+        event.record(&mut message);
 
-        if *level == LevelFilter::TRACE {
-            trace!("{visitor}")
-        } else if *level == LevelFilter::DEBUG {
-            debug!("{visitor}")
-        } else if *level == LevelFilter::INFO {
-            info!("{visitor}")
-        } else if *level == LevelFilter::WARN {
-            warn!("{visitor}")
-        } else if *level == LevelFilter::ERROR {
-            error!("{visitor}")
+        match *level {
+            Level::ERROR => {
+                error!("{message}");
+            }
+            Level::WARN => {
+                warn!("{message}");
+            }
+            Level::INFO => {
+                info!("{message}");
+            }
+            Level::DEBUG => {
+                debug!("{message}");
+            }
+            Level::TRACE => {
+                trace!("{message}");
+            }
         };
     }
 }
