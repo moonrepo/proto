5a50406cd03f01e2770231a343123f967b2c755c
Miles Johnson <milesj@users.noreply.github.com>
2025-07-16 18:09:07 -0700

    new: Bubble up `tracing` events from within WASM plugins. (#834)

--- crates/warpgate-pdk/src/lib.rs
+++ crates/warpgate-pdk/src/lib.rs
@@ -1,7 +1,11 @@
 mod api;
 mod funcs;
 mod macros;
+#[cfg(feature = "tracing")]
+mod tracing;
 
 pub use api::*;
 pub use funcs::*;
+#[cfg(feature = "tracing")]
+pub use tracing::*;
 pub use warpgate_api::*;
