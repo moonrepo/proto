795046ad8f08dd9bfc3261eac28177669c68bd90
Miles Johnson <mileswjohnson@gmail.com>
2025-09-13 21:55:57 -0700

    Fix tests.

--- crates/pdk-test-utils/src/config_builder.rs
+++ crates/pdk-test-utils/src/config_builder.rs
@@ -4,7 +4,9 @@ use warpgate::test_utils::ConfigBuilder;
 pub trait ProtoConfigBuilder {
     fn schema_config(&mut self, schema: serde_json::Value) -> &mut Self;
     fn backend_config(&mut self, config: impl Serialize) -> &mut Self;
+    fn backend_id(&mut self, id: String) -> &mut Self;
     fn tool_config(&mut self, config: impl Serialize) -> &mut Self;
+    fn tool_id(&mut self, id: String) -> &mut Self;
 }
 
 impl ProtoConfigBuilder for ConfigBuilder {
@@ -16,7 +18,16 @@ impl ProtoConfigBuilder for ConfigBuilder {
         self.insert("proto_backend_config", config)
     }
 
+    fn backend_id(&mut self, id: String) -> &mut Self {
+        self.insert("proto_backend_id", id)
+    }
+
     fn tool_config(&mut self, config: impl Serialize) -> &mut Self {
         self.insert("proto_tool_config", config)
+    }
+
+    fn tool_id(&mut self, id: String) -> &mut Self {
+        self.plugin_id(id.clone());
+        self.insert("proto_tool_id", id)
     }
 }
