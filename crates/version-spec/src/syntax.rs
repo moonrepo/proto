8cbfd9e9e488cbde3652740ad16967d7bcbed5dd
Miles Johnson <mileswjohnson@gmail.com>
2026-07-15 15:42:25 -0700

    Add display.

--- crates/version-spec/src/syntax.rs
+++ crates/version-spec/src/syntax.rs
@@ -1,3 +1,6 @@
+use serde::{Deserialize, Serialize};
+use std::fmt::{self, Display};
+
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct Version {
     pub scope: Option<String>,
@@ -6,6 +9,26 @@ pub struct Version {
     pub micro: u64, // day
     pub prerelease: Option<String>,
     pub build: Option<String>,
+}
+
+impl Display for Version {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if let Some(scope) = &self.scope {
+            write!(f, "{scope}-")?;
+        }
+
+        write!(f, "{}.{}.{}", self.major, self.minor, self.micro)?;
+
+        if let Some(pre) = &self.prerelease {
+            write!(f, "-{pre}")?;
+        }
+
+        if let Some(build) = &self.build {
+            write!(f, "+{build}")?;
+        }
+
+        Ok(())
+    }
 }
 
 #[derive(Copy, Clone, Debug, Default, Eq, PartialEq, Hash)]
@@ -22,6 +45,21 @@ pub enum Op {
     Wildcard,
 }
 
+impl Display for Op {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str(match self {
+            Op::Exact => "=",
+            Op::Greater => ">",
+            Op::GreaterEq => ">=",
+            Op::Less => "<",
+            Op::LessEq => "<=",
+            Op::Tilde => "~",
+            Op::Caret => "^",
+            Op::Wildcard => "",
+        })
+    }
+}
+
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct Requirement {
     pub op: Op,
@@ -33,6 +71,44 @@ pub struct Requirement {
     pub build: Option<String>,
 }
 
+impl Display for Requirement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.op)?;
+
+        if let Some(scope) = &self.scope {
+            write!(f, "{scope}-")?;
+        }
+
+        if let Some(major) = &self.major {
+            write!(f, "{major}")?;
+
+            if let Some(minor) = &self.minor {
+                write!(f, ".{minor}")?;
+
+                if let Some(micro) = &self.micro {
+                    write!(f, ".{micro}")?;
+                } else if self.op == Op::Wildcard {
+                    f.write_str(".*")?;
+                }
+            } else if self.op == Op::Wildcard {
+                f.write_str(".*")?;
+            }
+        } else if self.op == Op::Wildcard {
+            f.write_str(".*")?;
+        }
+
+        if let Some(pre) = &self.prerelease {
+            write!(f, "-{pre}")?;
+        }
+
+        if let Some(build) = &self.build {
+            write!(f, "+{build}")?;
+        }
+
+        Ok(())
+    }
+}
+
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Clause {
     And(Requirement, Requirement),
@@ -40,7 +116,35 @@ pub enum Clause {
     Only(Requirement),
 }
 
+impl Display for Clause {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Clause::And(req1, req2) => write!(f, "{req1} && {req2}"),
+            Clause::Between(ver1, ver2) => write!(f, "{ver1} - {ver2}"),
+            Clause::Only(req) => write!(f, "{req}"),
+        }
+    }
+}
+
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct Range {
     pub clauses: Vec<Clause>,
+}
+
+impl Display for Range {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.clauses.is_empty() {
+            return f.write_str("*");
+        }
+
+        for (i, clause) in self.clauses.iter().enumerate() {
+            if i > 0 {
+                f.write_str(" || ")?;
+            }
+
+            write!(f, "{clause}")?;
+        }
+
+        Ok(())
+    }
 }
